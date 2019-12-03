016f3bda49d18b3d805561a5b761a7d58a75dd16
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-03 21:25:57 +0100

    Day 3 part 2

--- src/main/kotlin/Day3CrossedWires.kt
+++ src/main/kotlin/Day3CrossedWires.kt
@@ -1,44 +1,61 @@
-import java.lang.Math.abs
+import kotlin.math.abs
 
 class Day3CrossedWires {
     fun manhattanDistanceToClosestIntersection(wire1: List<String>, wire2: List<String>): Int {
         val wire1Points = toPositions(wire1)
         val wire2Points = toPositions(wire2)
-        wire1Points.retainAll(wire2Points)
-        return wire1Points.stream()
+        val crossPositions = wire1Points.keys
+        crossPositions.retainAll(wire2Points.keys)
+        return crossPositions.stream()
             .mapToInt { abs(it.x) + abs(it.y) }
             .min()
             .orElse(0)
     }
 
-    private fun toPositions(wirePath: List<String>): HashSet<Position> {
-        val wire1Points = HashSet<Position>()
+    fun wireDistanceToClosestIntersection(wire1: List<String>, wire2: List<String>): Int {
+        val wire1Points = toPositions(wire1)
+        val wire2Points = toPositions(wire2)
+        val crossPositions = wire1Points.keys
+        crossPositions.retainAll(wire2Points.keys)
+        return crossPositions.stream()
+            .mapToInt { wire1Points.getOrDefault(it, 0) + wire2Points.getOrDefault(it, 0) }
+            .min()
+            .orElse(0)
+    }
+
+    private fun toPositions(wirePath: List<String>): HashMap<Position, Int> {
+        val wire1Points = HashMap<Position, Int>()
         var current = Position(x = 0, y = 0)
+        var travelledDistance = 0
         for (turn in wirePath) {
             val direction = turn[0]
             val steps = turn.substring(1).toInt()
             when (direction) {
                 'U' -> {
-                    for (y in current.y + 1 until current.y + steps) {
-                        wire1Points.add(Position(x = current.x, y = y))
+                    for (y in current.y + 1..current.y + steps) {
+                        travelledDistance++
+                        wire1Points.putIfAbsent(Position(x = current.x, y = y), travelledDistance)
                     }
                     current = Position(x = current.x, y = current.y + steps)
                 }
                 'D' -> {
-                    for (y in current.y - steps until current.y - 1) {
-                        wire1Points.add(Position(x = current.x, y = y))
+                    for (y in current.y - 1 downTo current.y - steps) {
+                        travelledDistance++
+                        wire1Points.putIfAbsent(Position(x = current.x, y = y), travelledDistance)
                     }
                     current = Position(x = current.x, y = current.y - steps)
                 }
                 'R' -> {
-                    for (x in current.x + 1 until current.x + steps) {
-                        wire1Points.add(Position(x = x, y = current.y))
+                    for (x in current.x + 1..current.x + steps) {
+                        travelledDistance++
+                        wire1Points.putIfAbsent(Position(x = x, y = current.y), travelledDistance)
                     }
                     current = Position(x = current.x + steps, y = current.y)
                 }
                 'L' -> {
-                    for (x in current.x - steps until current.x - 1) {
-                        wire1Points.add(Position(x = x, y = current.y))
+                    for (x in current.x - 1 downTo current.x - steps) {
+                        travelledDistance++
+                        wire1Points.putIfAbsent(Position(x = x, y = current.y), travelledDistance)
                     }
                     current = Position(x = current.x - steps, y = current.y)
                 }
--- src/test/kotlin/Day3CrossedWiresTest.kt
+++ src/test/kotlin/Day3CrossedWiresTest.kt
@@ -24,9 +24,30 @@ internal class Day3CrossedWiresTest {
     @Test
     fun manhattanDistanceToClosestIntersection() {
         val wires = Files.lines(Paths.get("./src/test/resources/day3.txt"))
-            .map { it.split(",")}
+            .map { it.split(",") }
             .toList()
         Assertions.assertEquals(855, Day3CrossedWires().manhattanDistanceToClosestIntersection(wires[0], wires[1]))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = ["R8,U5,L5,D3;U7,R6,D4,L4;30",
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72;U62,R66,U55,R34,D71,R55,D58,R83;610",
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51;U98,R91,D20,R16,D67,R40,U7,R15,U6,R7;410"], delimiter = ';'
+    )
+    fun wireDistanceToClosestIntersectionExamples(wire1: String, wire2: String, expected: Int) {
+        Assertions.assertEquals(
+            expected,
+            Day3CrossedWires().wireDistanceToClosestIntersection(wire1.split(","), wire2.split(","))
+        )
+    }
+
+    @Test
+    fun wireDistanceToClosestIntersection() {
+        val wires = Files.lines(Paths.get("./src/test/resources/day3.txt"))
+            .map { it.split(",") }
+            .toList()
+        Assertions.assertEquals(11238, Day3CrossedWires().wireDistanceToClosestIntersection(wires[0], wires[1]))
     }
 
 }
