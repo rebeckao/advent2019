2188699acfd40287c854836eca73cf9e1b958e0c
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-07 00:37:51 +0100

    Day 6 part 2

--- src/main/kotlin/Day6UniversalOrbit.kt
+++ src/main/kotlin/Day6UniversalOrbit.kt
@@ -1,27 +1,59 @@
+import java.util.*
+import kotlin.collections.HashMap
+
 class Day6UniversalOrbit {
     fun totalNumberOfOrbits(orbitStrings: List<String>): Int {
-        val orbits = HashMap<String, HeavenlyBody>()
-        for (orbit in orbitStrings) {
-            val bodies = orbit.split(")")
-            orbits[bodies[1]] = HeavenlyBody(bodies[0])
-        }
+        val orbits = mapOrbits(orbitStrings)
         return orbits.values.stream()
             .mapToInt { it.totalOrbitCount(orbits) }
             .sum()
     }
 
+    fun orbitTransfersBetweenYouAndSanta(orbitStrings: List<String>): Int {
+        val orbits = mapOrbits(orbitStrings)
+        val yourOrbitParents = orbits["YOU"]!!.parents(orbits)
+        val santasOrbitParents = orbits["SAN"]!!.parents(orbits)
+        for (parent in yourOrbitParents) {
+            if (santasOrbitParents.contains(parent)) {
+                return yourOrbitParents.indexOf(parent) + santasOrbitParents.indexOf(parent)
+            }
+        }
+        return 0
+    }
+
+    private fun mapOrbits(orbitStrings: List<String>): HashMap<String, HeavenlyBody> {
+        val orbits = HashMap<String, HeavenlyBody>()
+        for (orbit in orbitStrings) {
+            val bodies = orbit.split(")")
+            orbits[bodies[1]] = HeavenlyBody(bodies[0])
+        }
+        return orbits
+    }
+
     private class HeavenlyBody(private val orbitParent: String) {
         var orbitCount: Int? = null
+        var parents: LinkedList<String>? = null
 
         fun totalOrbitCount(orbits: HashMap<String, HeavenlyBody>): Int {
             if (orbitCount == null) {
-                if (orbitParent == "COM") {
-                    orbitCount = 1
+                orbitCount = if (orbitParent == "COM") {
+                    1
                 } else {
-                    orbitCount = 1 + orbits[orbitParent]!!.totalOrbitCount(orbits)
+                    1 + orbits[orbitParent]!!.totalOrbitCount(orbits)
                 }
             }
             return orbitCount!!
+        }
+
+        fun parents(orbits: HashMap<String, HeavenlyBody>): LinkedList<String> {
+            if (parents == null) {
+                parents = LinkedList()
+                parents!!.add(orbitParent)
+                if (orbitParent != "COM") {
+                    parents!!.addAll(orbits[orbitParent]!!.parents(orbits))
+                }
+            }
+            return parents!!
         }
     }
 }
--- src/test/kotlin/Day6UniversalOrbitTest.kt
+++ src/test/kotlin/Day6UniversalOrbitTest.kt
@@ -30,4 +30,30 @@ internal class Day6UniversalOrbitTest {
         val orbits = Files.lines(Paths.get("./src/test/resources/day6.txt")).toList()
         assertEquals(333679, Day6UniversalOrbit().totalNumberOfOrbits(orbits))
     }
+
+    @Test
+    fun orbitTransferExample() {
+        val orbits = listOf(
+            "COM)B",
+            "B)C",
+            "C)D",
+            "D)E",
+            "E)F",
+            "B)G",
+            "G)H",
+            "D)I",
+            "E)J",
+            "J)K",
+            "K)L",
+            "K)YOU",
+            "I)SAN"
+        )
+        assertEquals(4, Day6UniversalOrbit().orbitTransfersBetweenYouAndSanta(orbits))
+    }
+
+    @Test
+    fun orbitTransfers() {
+        val orbits = Files.lines(Paths.get("./src/test/resources/day6.txt")).toList()
+        assertEquals(370, Day6UniversalOrbit().orbitTransfersBetweenYouAndSanta(orbits))
+    }
 }
