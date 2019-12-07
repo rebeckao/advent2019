bd71a26994037711c95833e2832d0c83c98f928d
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-07 19:32:17 +0100

    Day 7 part 2

--- src/main/kotlin/Day7AmplificationCircuit.kt
+++ src/main/kotlin/Day7AmplificationCircuit.kt
@@ -19,7 +19,43 @@ class Day7AmplificationCircuit {
         return maxOutput
     }
 
-    private fun possiblePhaseSettings(possibleSettings: Set<Int>): MutableList<IntArray> {
+    fun maxThrusterSignalWithFeedback(program: IntArray): Int {
+        val phaseSettings: List<IntArray> = possiblePhaseSettings(setOf(5, 6, 7, 8, 9))
+        var maxOutput = 0
+        for (phaseSetting in phaseSettings) {
+            val output = maxThrusterSignalWithFeedback(program.clone(), phaseSetting)
+            maxOutput = max(maxOutput, output)
+        }
+        return maxOutput
+    }
+
+    fun maxThrusterSignalWithFeedback(program: IntArray, phaseSettings: IntArray): Int {
+        val programs: MutableList<IntArray> =
+            mutableListOf(program.clone(), program.clone(), program.clone(), program.clone(), program.clone())
+        val inputQueues: List<Queue<Int>> = phaseSettings.asSequence().map{toQueue(it)}.toList()
+        val positions = intArrayOf(0, 0, 0, 0, 0)
+        var output = 0
+        while (true) {
+            for (i in 0 until programs.size) {
+                inputQueues[i].add(output)
+                val nextOutput = nextOutput(programs[i], inputQueues[i], positions[i])
+                if (nextOutput == null) {
+                    return output
+                } else {
+                    output = nextOutput.first
+                    positions[i] = nextOutput.second
+                }
+            }
+        }
+    }
+
+    private fun toQueue(value: Int): ArrayDeque<Int> {
+        val queue = ArrayDeque<Int>()
+        queue.add(value)
+        return queue
+    }
+
+    fun possiblePhaseSettings(possibleSettings: Set<Int>): MutableList<IntArray> {
         val phaseSettings: MutableList<IntArray> = ArrayList()
         for (phase1 in possibleSettings) {
             for (phase2 in possibleSettings) {
@@ -37,5 +73,84 @@ class Day7AmplificationCircuit {
             }
         }
         return phaseSettings
+    }
+
+    private fun nextOutput(program: IntArray, input: Queue<Int>, startPosition : Int): Pair<Int,Int>? {
+        var currentPosition = startPosition
+        while (currentPosition < program.size) {
+            val opCodeString = String.format("%05d", program[currentPosition])
+            val currentOperation = opCodeString.substring(3)
+
+            if (currentOperation == "99") {
+                return null
+            }
+
+            if (currentOperation == "03") {
+                if (input.size == 0) {
+                    println("currentPosition = ${currentPosition}")
+                }
+                program[program[currentPosition + 1]] = input.poll()
+                currentPosition += 2
+                continue
+            }
+
+            val param1 =
+                if (opCodeString[2] == '1') program[currentPosition + 1] else program[program[currentPosition + 1]]
+
+            if (currentOperation == "04") {
+                currentPosition+=2
+                return Pair(param1, currentPosition)
+            }
+
+            val param2 =
+                if (opCodeString[1] == '1') program[currentPosition + 2] else program[program[currentPosition + 2]]
+
+            if (currentOperation == "05") {
+                if (param1 != 0) {
+                    currentPosition = param2
+                } else {
+                    currentPosition += 3
+                }
+                continue
+            }
+
+            if (currentOperation == "06") {
+                if (param1 == 0) {
+                    currentPosition = param2
+                } else {
+                    currentPosition += 3
+                }
+                continue
+            }
+
+            val resultPos = program[currentPosition + 3]
+
+            if (currentOperation == "07") {
+                program[resultPos] = if (param1 < param2) 1 else 0
+                currentPosition += 4
+                continue
+            }
+
+            if (currentOperation == "08") {
+                program[resultPos] = if (param1 == param2) 1 else 0
+                currentPosition += 4
+                continue
+            }
+
+            if (currentOperation == "01") {
+                program[resultPos] = param1 + param2
+                currentPosition += 4
+                continue
+            }
+
+            if (currentOperation == "02") {
+                program[resultPos] = param1 * param2
+                currentPosition += 4
+                continue
+            }
+
+            throw IllegalStateException("op code = ${currentOperation}, at position $currentPosition")
+        }
+        throw IllegalStateException("No output and no halt")
     }
 }
