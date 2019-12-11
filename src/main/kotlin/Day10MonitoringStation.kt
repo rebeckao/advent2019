d7f314a683d7a7ca2cfec09e48d8c63e32ddc9d4
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-11 22:48:19 +0100

    Day 10 part 2

--- src/main/kotlin/Day10MonitoringStation.kt
+++ src/main/kotlin/Day10MonitoringStation.kt
@@ -1,8 +1,16 @@
-
+import kotlin.math.PI
 import kotlin.math.abs
+import kotlin.math.atan
 
 class Day10MonitoringStation {
     fun maxNumberOfOthersAsteroidsDetected(map: List<String>): Int {
+        val asteroidPositions = toPositions(map)
+        return asteroidPositions
+            .map { numberOfOthersAsteroidsDetected(it, asteroidPositions) }
+            .max() ?: 0
+    }
+
+    private fun toPositions(map: List<String>): MutableSet<Vector> {
         val asteroidPositions = HashSet<Vector>().toMutableSet()
         for (y in map.indices) {
             val row = map[y]
@@ -13,20 +21,91 @@ class Day10MonitoringStation {
             }
         }
         return asteroidPositions
-            .map { numberOfOthersAsteroidsDetected(it, asteroidPositions) }
-            .max() ?: 0
     }
 
     private fun numberOfOthersAsteroidsDetected(vector: Vector, asteroidVectors: MutableSet<Vector>): Int {
-        return asteroidVectors
-            .filter { it != vector }
-            .map { relativePosition(it, vector) }
+        return relativeDirectionsToOtherAsteroids(vector, asteroidVectors)
             .map { toDirection(it) }
             .distinct()
             .size
     }
 
-    private fun toDirection(vector: Vector): Any {
+    fun asteroidDestroyed(map: List<String>, asteroidNumber: Int): Vector {
+        val asteroidPositions = toPositions(map)
+        val laserPosition = asteroidPositions.maxBy { numberOfOthersAsteroidsDetected(it, asteroidPositions) }!!
+        asteroidPositions.remove(laserPosition)
+        val leftToDestroy = relativeDirectionsToOtherAsteroids(laserPosition, asteroidPositions)
+        var latestDestroyed = Vector(0, 0)
+        var laserDirection = Vector(x = -1, y = -(map.size + 1))
+        var numberOfDestroyed = 0
+        while (leftToDestroy.isNotEmpty()) {
+            latestDestroyed = asteroidPositions
+                .groupBy { relativeDirection(observer = laserPosition, observed = it) }
+                .minBy { rotationDistanceBetween(laserDirection, it.key) }!!
+                .value
+                .minBy { manhattanDistance(laserPosition, it) }!!
+            laserDirection = relativeDirection(observer = laserPosition, observed = latestDestroyed)
+            asteroidPositions.remove(latestDestroyed)
+            numberOfDestroyed++
+            if (numberOfDestroyed == asteroidNumber) {
+                return latestDestroyed
+            }
+        }
+        return latestDestroyed
+    }
+
+    private fun relativeDirection(
+        observer: Vector,
+        observed: Vector
+    ) = toDirection(relativePosition(observer = observer, observed = observed))
+
+    private fun manhattanDistance(
+        pos1: Vector,
+        pos2: Vector
+    ): Int {
+        val vector = relativePosition(pos1, pos2)
+        return abs(vector.x) + abs(vector.y)
+    }
+
+    private fun rotationDistanceBetween(laserDirection: Vector, asteroidDirection: Vector): Double {
+        if (laserDirection == asteroidDirection) {
+            return 2 * PI
+        }
+        val laserDistance = radians(asteroidDirection)
+        val asteroidDistance = radians(laserDirection)
+        val difference = laserDistance - asteroidDistance
+        return if (difference > 0) {
+            difference
+        } else {
+            difference + 2 * PI
+        }
+    }
+
+    fun radians(direction: Vector): Double {
+        val x = direction.x
+        val y = direction.y
+        if (x >= 0 && y < 0) {
+            return atan(x.toDouble() / -y.toDouble())
+        } else if (x > 0 && y >= 0) {
+            return atan(y.toDouble() / x.toDouble()) + PI / 2
+        } else if (x <= 0 && y > 0) {
+            return atan(-x.toDouble() / y.toDouble()) + PI
+        } else if (x < 0 && y <= 0) {
+            return atan(y.toDouble() / x.toDouble()) + 3 * PI / 2
+        }
+        throw IllegalStateException()
+    }
+
+    private fun relativeDirectionsToOtherAsteroids(
+        vector: Vector,
+        asteroidVectors: MutableSet<Vector>
+    ): List<Vector> {
+        return asteroidVectors
+            .filter { it != vector }
+            .map { relativePosition(it, vector) }
+    }
+
+    private fun toDirection(vector: Vector): Vector {
         val divisor = greatestCommonDivisorPreservingSign(vector.x, vector.y)
         return Vector(x = vector.x / divisor, y = vector.y / divisor)
     }
--- src/test/kotlin/Day10MonitoringStationTest.kt
+++ src/test/kotlin/Day10MonitoringStationTest.kt
@@ -1,9 +1,12 @@
+import Day10MonitoringStation.Vector
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.math.PI
+import kotlin.math.atan
 import kotlin.streams.toList
 
 internal class Day10MonitoringStationTest {
@@ -73,12 +76,93 @@ internal class Day10MonitoringStationTest {
     @Test
     fun maxNumberOfOthersAsteroidsDetected() {
         val map = Files.lines(Paths.get("./src/test/resources/day10.txt")).toList()
-        assertEquals(0, Day10MonitoringStation().maxNumberOfOthersAsteroidsDetected(map))
+        assertEquals(282, Day10MonitoringStation().maxNumberOfOthersAsteroidsDetected(map))
     }
 
     @ParameterizedTest
     @CsvSource("2,4,2", "3,5,1", "12,6,6", "-1,-2,1", "-5,-10,5")
     fun greatestCommonDivisor(a: Int, b: Int, expected: Int) {
         assertEquals(expected, Day10MonitoringStation().greatestCommonDivisorPreservingSign(a, b))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,8,1",
+        "9,15,1"
+    )
+    fun asteroidToBeVaporizedSmallExample(asteroidNumber: Int, expectedX: Int, expectedY: Int) {
+        val map: List<String> = (".#....#####...#..\n" +
+                "##...##.#####..##\n" +
+                "##...#...#.#####.\n" +
+                "..#.....#...###..\n" +
+                "..#.#.....#....##").split("\n")
+        assertEquals(
+            Vector(x = expectedX, y = expectedY),
+            Day10MonitoringStation().asteroidDestroyed(map, asteroidNumber)
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,11,12",
+        "2,12,1",
+        "3,12,2",
+        "10,12,8",
+        "20,16,0",
+        "50,16,9",
+        "100,10,16",
+        "199,9,6",
+        "200,8,2",
+        "201,10,9",
+        "299,11,1"
+    )
+    fun asteroidToBeVaporizedLargeExample(asteroidNumber: Int, expectedX: Int, expectedY: Int) {
+        val map: List<String> = (".#..##.###...#######\n" +
+                "##.############..##.\n" +
+                ".#.######.########.#\n" +
+                ".###.#######.####.#.\n" +
+                "#####.##.#.##.###.##\n" +
+                "..#####..#.#########\n" +
+                "####################\n" +
+                "#.####....###.#.#.##\n" +
+                "##.#################\n" +
+                "#####.##.###..####..\n" +
+                "..######..##.#######\n" +
+                "####.##.####...##..#\n" +
+                ".#####..#.######.###\n" +
+                "##...#.##########...\n" +
+                "#.##########.#######\n" +
+                ".####.#.###.###.#.##\n" +
+                "....##.##.###..#####\n" +
+                ".#.#.###########.###\n" +
+                "#.#.#.#####.####.###\n" +
+                "###.##.####.##.#..##").split("\n")
+        assertEquals(
+            Vector(x = expectedX, y = expectedY),
+            Day10MonitoringStation().asteroidDestroyed(map, asteroidNumber)
+        )
+    }
+
+    @Test
+    fun asteroidToBeVaporized() {
+        val map = Files.lines(Paths.get("./src/test/resources/day10.txt")).toList()
+        val asteroidDestroyed = Day10MonitoringStation().asteroidDestroyed(map, 200)
+        assertEquals(1008, asteroidDestroyed.x * 100 + asteroidDestroyed.y)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0,-1,0",
+        "1,-1,0.25",
+        "1,0,0.5",
+        "1,1,0.75",
+        "0,1,1",
+        "-1,1,1.25",
+        "-1,0,1.5",
+        "-1,-1,1.75"
+    )
+    fun radians(x: Int, y: Int, expectedNumberOfPies: Double) {
+        println(atan(0.0))
+        assertEquals(expectedNumberOfPies * PI, Day10MonitoringStation().radians(Vector(x = x, y = y)))
     }
 }
