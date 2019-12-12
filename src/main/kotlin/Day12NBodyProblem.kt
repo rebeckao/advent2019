267b5816ca05c547c23b760a68d3d3304fb703d9
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-12 19:51:04 +0100

    IntArrays instead

--- src/main/kotlin/Day12NBodyProblem.kt
+++ src/main/kotlin/Day12NBodyProblem.kt
@@ -11,9 +11,10 @@ class Day12NBodyProblem {
     }
 
     fun stepsToRepeat(moonsStrings: List<String>): Long {
-        val moons = moonsStrings.map { Moon(it) }.toSet()
+        val moons = moonsStrings.map { Moon(it) }.toList()
         val states = HashSet<String>().toMutableSet()
         states.add(hash(moons))
+        val start = System.currentTimeMillis()
         for (step in 1 until Long.MAX_VALUE) {
             moons.forEach { it.updateVelocity(moons.map { it.position }) }
             moons.forEach { it.updatePosition() }
@@ -22,66 +23,67 @@ class Day12NBodyProblem {
                 return step
             }
             states.add(hash)
+            if (step % 100_000L == 0L) {
+                println("System.currentTimeMillis() = ${(System.currentTimeMillis() - start)}")
+            }
         }
         return 0
     }
 
-    private fun hash(moons: Set<Moon>) =
+    private fun hash(moons: List<Moon>) =
         moons.map { it.hashCode() }.joinToString("")
 
-    data class Moon(var position: threeDVector, var velocity: threeDVector) {
+    data class Moon(var position: IntArray, var velocity: IntArray) {
 
-        constructor(str: String) : this(threeDVector(0, 0, 0), threeDVector(0, 0, 0)) {
+        constructor(str: String) : this(intArrayOf(0,0,0), intArrayOf(0,0,0)) {
             this.position = parsePosition(str)
         }
 
-        private fun parsePosition(str: String): threeDVector {
+        private fun parsePosition(str: String): IntArray {
             val vectorPattern = Regex("<x=(.*), y=(.*), z=(.*)>")
             val match = vectorPattern.matchEntire(str)
             val x = match!!.groupValues[1].toInt()
             val y = match.groupValues[2].toInt()
             val z = match.groupValues[3].toInt()
-            return threeDVector(x, y, z)
+            return intArrayOf(x, y, z)
         }
 
         fun kineticEnergy(): Int {
-            return abs(velocity.x) + abs(velocity.y) + abs(velocity.z)
+            return abs(velocity[0]) + abs(velocity[1]) + abs(velocity[2])
         }
 
         fun potentialEnergy(): Int {
-            return abs(position.x) + abs(position.y) + abs(position.z)
+            return abs(position[0]) + abs(position[1]) + abs(position[2])
         }
 
-        fun updateVelocity(positions: List<threeDVector>) {
+        fun updateVelocity(positions: List<IntArray>) {
             val xDelta = positions.stream()
-                .filter{it != position}
-                .map { it.x }
-                .map { it - position.x }
+                .filter{ !it!!.contentEquals(position) }
+                .map { it[0] }
+                .map { it - position[0] }
                 .mapToInt { if (it == 0) 0 else it / abs(it) }
                 .sum()
             val yDelta = positions.stream()
-                .filter{it != position}
-                .map { it.y }
-                .map { it - position.y }
+                .filter{ !it!!.contentEquals(position) }
+                .map { it[1] }
+                .map { it - position[1] }
                 .mapToInt { if (it == 0) 0 else it / abs(it) }
                 .sum()
             val zDelta = positions.stream()
-                .filter{it != position}
-                .map { it.z }
-                .map { it - position.z }
+                .filter{ !it!!.contentEquals(position) }
+                .map { it[2] }
+                .map { it - position[2] }
                 .mapToInt { if (it == 0) 0 else it / abs(it) }
                 .sum()
-            velocity.x += xDelta
-            velocity.y += yDelta
-            velocity.z += zDelta
+            velocity[0] += xDelta
+            velocity[1] += yDelta
+            velocity[2] += zDelta
         }
 
         fun updatePosition() {
-            position.x += velocity.x
-            position.y += velocity.y
-            position.z += velocity.z
+            position[0] += velocity[0]
+            position[1] += velocity[1]
+            position[2] += velocity[2]
         }
     }
-
-    data class threeDVector(var x: Int, var y: Int, var z: Int)
 }
