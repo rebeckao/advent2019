02eceb0e6d31feacb9315edd15cdb48c2771aae9
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-14 11:45:05 +0100

    Day 12 part 2

--- src/main/kotlin/Day12NBodyProblem.kt
+++ src/main/kotlin/Day12NBodyProblem.kt
@@ -2,20 +2,12 @@ import Util.Companion.greatestCommonDivisorPreservingSign
 import kotlin.math.abs
 
 class Day12NBodyProblem {
-    fun totalEnergy(moonsStrings: List<String>, steps: Int): Int {
-        val moons = moonsStrings.map { Moon(it) }.toSet()
-        for (step in 0 until steps) {
-            moons.forEach { it.updateVelocity(moons.map { it.position }) }
-            moons.forEach { it.updatePosition() }
-        }
-        return moons.stream().mapToInt { it.kineticEnergy() * it.potentialEnergy() }.sum()
-    }
 
     fun totalEnergyDecoupled(moonsStrings: List<String>, steps: Int): Int {
-        val moons = moonsStrings.map { Moon(it) }.toSet()
-        val xStateAfterSteps = stateAfterSteps(moons.map { it.position[0] }.toIntArray(), steps)
-        val yStateAfterSteps = stateAfterSteps(moons.map { it.position[1] }.toIntArray(), steps)
-        val zStateAfterSteps = stateAfterSteps(moons.map { it.position[2] }.toIntArray(), steps)
+        val moons = moonsStrings.map { parsePosition(it) }.toSet()
+        val xStateAfterSteps = stateAfterSteps(moons.map { it[0] }.toIntArray(), steps)
+        val yStateAfterSteps = stateAfterSteps(moons.map { it[1] }.toIntArray(), steps)
+        val zStateAfterSteps = stateAfterSteps(moons.map { it[2] }.toIntArray(), steps)
         var sum = 0
         for (i in 0..3) {
             val potential =
@@ -38,23 +30,20 @@ class Day12NBodyProblem {
     }
 
     fun stepsToRepeatDecoupled(moonsStrings: List<String>): Long {
-        val moons = moonsStrings.map { Moon(it) }.toList()
-        val stepsToRepeatForX =
-            stepsToRepeatForDimension(moons.map { it.position[0] }.toIntArray(), intArrayOf(0, 0, 0, 0))
-        println("stepsToRepeatForX = ${stepsToRepeatForX}")
-        val stepsToRepeatForY =
-            stepsToRepeatForDimension(moons.map { it.position[1] }.toIntArray(), intArrayOf(0, 0, 0, 0))
-        println("stepsToRepeatForY = ${stepsToRepeatForY}")
-        val stepsToRepeatForZ =
-            stepsToRepeatForDimension(moons.map { it.position[2] }.toIntArray(), intArrayOf(0, 0, 0, 0))
-        println("stepsToRepeatForZ = ${stepsToRepeatForZ}")
-        val xAndY = stepsToRepeatForX * stepsToRepeatForY /
-                greatestCommonDivisorPreservingSign(stepsToRepeatForX, stepsToRepeatForY)
+        val moons = moonsStrings.map { parsePosition(it) }.toList()
+        val stepsToRepeatForX = stepsToRepeatForDimension(moons.map { it[0] }.toIntArray())
+        val stepsToRepeatForY = stepsToRepeatForDimension(moons.map { it[1] }.toIntArray())
+        val stepsToRepeatForZ = stepsToRepeatForDimension(moons.map { it[2] }.toIntArray())
+        val xAndY = stepsToRepeatForX * stepsToRepeatForY / greatestCommonDivisorPreservingSign(
+            stepsToRepeatForX,
+            stepsToRepeatForY
+        )
         val xAndYAndZ = xAndY * stepsToRepeatForZ / greatestCommonDivisorPreservingSign(xAndY, stepsToRepeatForZ)
         return xAndYAndZ
     }
 
-    private fun stepsToRepeatForDimension(positions: IntArray, velocities: IntArray): Long {
+    fun stepsToRepeatForDimension(positions: IntArray): Long {
+        val velocities = intArrayOf(0, 0, 0, 0)
         val states = HashSet<String>().toMutableSet()
         states.add(hash(Pair(positions, velocities)))
         for (step in 1 until Long.MAX_VALUE) {
@@ -77,57 +66,15 @@ class Day12NBodyProblem {
         }
     }
 
-    private fun hash(state: Pair<IntArray, IntArray>) =
-        "" + state.first.contentHashCode() + state.second.contentHashCode()
-
-    data class Moon(var position: IntArray, var velocity: IntArray) {
-
-        constructor(str: String) : this(intArrayOf(0, 0, 0), intArrayOf(0, 0, 0)) {
-            this.position = parsePosition(str)
-        }
-
-        private fun parsePosition(str: String): IntArray {
-            val vectorPattern = Regex("<x=(.*), y=(.*), z=(.*)>")
-            val match = vectorPattern.matchEntire(str)
-            val x = match!!.groupValues[1].toInt()
-            val y = match.groupValues[2].toInt()
-            val z = match.groupValues[3].toInt()
-            return intArrayOf(x, y, z)
-        }
-
-        fun kineticEnergy(): Int {
-            return abs(velocity[0]) + abs(velocity[1]) + abs(velocity[2])
-        }
-
-        fun potentialEnergy(): Int {
-            return abs(position[0]) + abs(position[1]) + abs(position[2])
-        }
-
-        fun updateVelocity(positions: List<IntArray>) {
-            val xDelta = positions.stream()
-                .filter { !it!!.contentEquals(position) }
-                .map { it[0] }
-                .mapToInt { it.compareTo(position[0]) }
-                .sum()
-            val yDelta = positions.stream()
-                .filter { !it!!.contentEquals(position) }
-                .map { it[1] }
-                .mapToInt { it.compareTo(position[1]) }
-                .sum()
-            val zDelta = positions.stream()
-                .filter { !it!!.contentEquals(position) }
-                .map { it[2] }
-                .mapToInt { it.compareTo(position[2]) }
-                .sum()
-            velocity[0] += xDelta
-            velocity[1] += yDelta
-            velocity[2] += zDelta
-        }
-
-        fun updatePosition() {
-            position[0] += velocity[0]
-            position[1] += velocity[1]
-            position[2] += velocity[2]
-        }
+    private fun parsePosition(str: String): IntArray {
+        val vectorPattern = Regex("<x=(.*), y=(.*), z=(.*)>")
+        val match = vectorPattern.matchEntire(str)
+        val x = match!!.groupValues[1].toInt()
+        val y = match.groupValues[2].toInt()
+        val z = match.groupValues[3].toInt()
+        return intArrayOf(x, y, z)
     }
+
+    private fun hash(state: Pair<IntArray, IntArray>) =
+        "" + state.first.joinToString("") + state.second.joinToString("")
 }
--- src/test/kotlin/Day12NBodyProblemTest.kt
+++ src/test/kotlin/Day12NBodyProblemTest.kt
@@ -21,14 +21,12 @@ internal class Day12NBodyProblemTest {
         delimiter = ';'
     )
     fun totalEnergyExamples(moons: String, steps: Int, expected: Int) {
-        assertEquals(expected, Day12NBodyProblem().totalEnergy(moons.split("\\n"), steps))
         assertEquals(expected, Day12NBodyProblem().totalEnergyDecoupled(moons.split("\\n"), steps))
     }
 
     @Test
     fun totalEnergy() {
         val moons = Files.lines(Paths.get("./src/test/resources/day12.txt")).toList()
-        assertEquals(8538, Day12NBodyProblem().totalEnergy(moons, 1000))
         assertEquals(8538, Day12NBodyProblem().totalEnergyDecoupled(moons, 1000))
     }
 
@@ -37,12 +35,15 @@ internal class Day12NBodyProblemTest {
         value = ["<x=-1, y=0, z=2>\\n" +
                 "<x=2, y=-10, z=-7>\\n" +
                 "<x=4, y=-8, z=8>\\n" +
-                "<x=3, y=5, z=-1>;2772"
-            ,
+                "<x=3, y=5, z=-1>;2772",
             "<x=-8, y=-10, z=0>\\n" +
                     "<x=5, y=5, z=10>\\n" +
                     "<x=2, y=-7, z=3>\\n" +
-                    "<x=9, y=-8, z=-3>;4686774924"
+                    "<x=9, y=-8, z=-3>;4686774924",
+            "<x=-4, y=-9, z=-3>\\n" +
+                    "<x=-13, y=-11, z=0>\\n" +
+                    "<x=-17, y=-7, z=15>\\n" +
+                    "<x=-16, y=4, z=2>;548525804273976"
         ],
         delimiter = ';'
     )
@@ -50,9 +51,25 @@ internal class Day12NBodyProblemTest {
         assertEquals(expected, Day12NBodyProblem().stepsToRepeatDecoupled(moons.split("\\n")))
     }
 
+    @ParameterizedTest
+    @CsvSource(
+        "-1,2,4,3,18",
+        "0,-10,-8,5,28",
+        "2,-7,8,-1,44",
+        "-8,5,2,9,2028",
+        "-10,5,-7,-8,5898",
+        "0,10,3,-3,4702",
+        "-4,-13,-17,-16,113028",
+        "-9,-11,-7,4,167624",
+        "-3,0,15,2,231614"
+    )
+    fun stepsToRepeatForDimension(pos0: Int, pos1: Int, pos2: Int, pos3: Int, expected: Long) {
+        assertEquals(expected, Day12NBodyProblem().stepsToRepeatForDimension(intArrayOf(pos0, pos1, pos2, pos3)))
+    }
+
     @Test
     fun stepsToRepeat() {
         val moons = Files.lines(Paths.get("./src/test/resources/day12.txt")).toList()
-        assertEquals(978349897231606, Day12NBodyProblem().stepsToRepeatDecoupled(moons))
+        assertEquals(506359021038056, Day12NBodyProblem().stepsToRepeatDecoupled(moons))
     }
 }
