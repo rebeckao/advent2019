56d9bf517fbecc931cd7bf246a6d68e000120599
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-21 23:43:08 +0100

    Day 18 part 2

--- src/main/kotlin/Day18ManyWorlds.kt
+++ src/main/kotlin/Day18ManyWorlds.kt
@@ -1,75 +1,111 @@
+
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 import kotlin.collections.HashSet
 
-class Day18ManyWorlds(val map: List<String>) {
-    val paths: Map<Char, List<PathToKey>>
+class Day18ManyWorlds(rawMap: List<String>) {
+    private val paths: Map<Char, List<PathToKey>>
     private val shortestPaths = HashMap<String, Int>().toMutableMap()
+    private val numberOfKeys: Int
+    private val startPaths: List<List<PathToKey>>
+    private val map: List<String>
 
     init {
-        this.paths = toPaths(map)
-    }
-
-    fun shortestPathToCollectAllKeys(): Int {
-        val allKeyNodes = HashMap<Char, KeyNode>().toMutableMap()
-        paths.filter { it.key != '@' }
-            .map { KeyNode(it.key, it.value, shortestPaths) }
-            .forEach { allKeyNodes[it.key] = it }
-        val shortestPathToRemainingKeys =
-            KeyNode('@', paths['@']!!, shortestPaths).shortestPathToRemainingKeys(emptySet(), allKeyNodes)
-        return shortestPathToRemainingKeys
-    }
-
-    private fun toPaths(initialMap: List<String>): Map<Char, List<PathToKey>> {
         val allKeys = HashMap<Char, Pair<Int, Int>>()
-        var startingPosition = Pair(0, 0)
-        for (y in initialMap.indices) {
-            for (x in initialMap[y].indices) {
-                if (initialMap[y][x] == '@') {
-                    startingPosition = Pair(x, y)
-                }
-                if (initialMap[y][x].toString().matches(Regex("[a-z]"))) {
-                    allKeys[initialMap[y][x]] = Pair(x, y)
+        val startingPositions = ArrayList<Pair<Int, Int>>()
+        for (y in rawMap.indices) {
+            for (x in rawMap[y].indices) {
+                if (rawMap[y][x] == '@') {
+                    startingPositions.add(Pair(x, y))
+                } else {
+                    if (rawMap[y][x].toString().matches(Regex("[a-z]"))) {
+                        allKeys[rawMap[y][x]] = Pair(x, y)
+                    }
                 }
             }
         }
-        val map = initialMap.map { it.replace('@', '.') }
+        this.numberOfKeys = allKeys.size
+        this.map = rawMap.map { it.replace('@', '.') }
         val paths = HashMap<Char, List<PathToKey>>().toMutableMap()
-        paths['@'] = findPaths('@', map, startingPosition, allKeys.keys)
-        for (key in allKeys.keys) {
-            paths[key] = findPaths(key, map, allKeys[key]!!, allKeys.keys)
+        this.startPaths = startingPositions.map { findPaths(it) }
+        startPaths.indices.forEach {
+            paths[it.toString()[0]] = findPaths(startingPositions[it])
         }
-        return paths
+        for (key in allKeys.keys) {
+            paths[key] = findPaths(allKeys[key]!!)
+        }
+        this.paths = paths
+    }
+
+    fun shortestPathToCollectAllKeys(): Int {
+        val startingTokens = startPaths.indices.map { it.toString()[0] }.toCharArray()
+        return shortestPathToRemainingKeys(emptySet(), startingTokens)
+    }
+
+    private fun shortestPathToRemainingKeys(keysCollected: Set<Char>, currentKeys: CharArray): Int {
+        if (keysCollected.size == numberOfKeys) {
+            return 0
+        }
+        val hash = hash(currentKeys, keysCollected)
+        var minSteps = Int.MAX_VALUE
+        if (!shortestPaths.containsKey(hash)) {
+            for (keyIndex in currentKeys.indices) {
+                val possibePaths = paths[currentKeys[keyIndex]]!!
+                    .filter { !keysCollected.contains(it.to) }
+                    .filter { theWayIsCleared(it.blockedBy, keysCollected) }
+                if (possibePaths.isEmpty()) {
+                    continue
+                }
+                val shortestPath = possibePaths
+                    .map {
+                        val newCurrentKeys = currentKeys.clone()
+                        newCurrentKeys[keyIndex] = it.to
+                        it.distance + shortestPathToRemainingKeys(collectKey(keysCollected, it.to), newCurrentKeys)
+                    }
+                    .min()!!
+                if (shortestPath < minSteps) {
+                    minSteps = shortestPath
+                }
+            }
+            shortestPaths[hash] = minSteps
+        }
+        return shortestPaths[hash]!!
+    }
+
+    private fun collectKey(keysCollectedIncoming: Set<Char>, newKey: Char): Set<Char> {
+        val keysCollected = keysCollectedIncoming.toMutableSet()
+        keysCollected.add(newKey)
+        return keysCollected
+    }
+
+    private fun theWayIsCleared(blockers: List<Char>, keysCollected: Set<Char>): Boolean {
+        return blockers.filter { !keysCollected.contains(it) }.isEmpty()
+    }
+
+    private fun hash(currentKeys: CharArray, keysCollected: Set<Char>): String {
+        return "${currentKeys.sorted().joinToString(".")}-${keysCollected.sorted().joinToString(".")}"
     }
 
     private fun findPaths(
-        startingKey: Char,
-        initialMap: List<String>,
-        startPosition: Pair<Int, Int>,
-        allKeys: MutableSet<Char>
+        startPosition: Pair<Int, Int>
     ): List<PathToKey> {
         val foundPaths = ArrayList<PathToKey>().toMutableList()
-        val keysToFind = allKeys.filter { it != startingKey }
         val blockerPattern = Regex("[A-Z]")
         val keyPattern = Regex("[a-z]")
         val explorations = ArrayDeque<PathState>() // Breadth first
         val visitedPositions = HashSet<Pair<Int, Int>>()
         visitedPositions.add(startPosition)
         explorations.add(PathState(startPosition, 0, Collections.emptyList()))
-        while (explorations.isNotEmpty() && foundPaths.size < keysToFind.size) {
+        while (explorations.isNotEmpty() && foundPaths.size < numberOfKeys) {
             val pathState = explorations.poll()!!
             val currentPosition = pathState.position
-//            val currentTile = initialMap[currentPosition.second][currentPosition.first]
-//            if (currentTile.toString().matches(keyPattern)) {
-//                foundPaths.add(PathToKey(currentTile, pathState.steps, pathState.blockers))
-//            }
             for (dir in Arrays.asList(Pair(0, -1), Pair(0, 1), Pair(-1, 0), Pair(1, 0))) {
                 val newPosition = Pair(currentPosition.first + dir.first, currentPosition.second + dir.second)
                 if (visitedPositions.contains(newPosition)) {
                     continue
                 }
-                val tile = initialMap[newPosition.second][newPosition.first]
+                val tile = map[newPosition.second][newPosition.first]
                 if (tile == '#') {
                     continue
                 }
@@ -94,41 +130,5 @@ class Day18ManyWorlds(val map: List<String>) {
         val steps: Int,
         val blockers: List<Char>
     )
-
-    class KeyNode(
-        val key: Char,
-        private val pathsAway: List<PathToKey>,
-        private val shortestPaths: MutableMap<String, Int>
-    ) {
-
-        fun shortestPathToRemainingKeys(keysCollectedIncoming: Set<Char>, allKeyNodes: Map<Char, KeyNode>): Int {
-            val keysCollected = keysCollectedIncoming.toMutableSet()
-            if (key != '@') {
-                keysCollected.add(key)
-            }
-            if (keysCollected.size == allKeyNodes.size) {
-                return 0
-            }
-            val hash = hash(key, keysCollected)
-            if (!shortestPaths.containsKey(hash)) {
-                val shortestPath = pathsAway
-                    .filter { !keysCollected.contains(it.to) }
-                    .filter { theWayIsCleared(it.blockedBy, keysCollected) }
-                    .map { it.distance + allKeyNodes[it.to]!!.shortestPathToRemainingKeys(keysCollected, allKeyNodes) }
-                    .min()!!
-                shortestPaths[hash] = shortestPath
-            }
-            val shortest = shortestPaths[hash]!!
-            return shortest
-        }
-
-        private fun theWayIsCleared(blockers: List<Char>, keysCollected: Set<Char>): Boolean {
-            return blockers.filter { !keysCollected.contains(it) }.isEmpty()
-        }
-
-        private fun hash(key: Char, keysCollected: Set<Char>): String {
-            return "$key-${keysCollected.sorted().joinToString(".")}"
-        }
-    }
 
 }
--- src/test/kotlin/Day18ManyWorldsTest.kt
+++ src/test/kotlin/Day18ManyWorldsTest.kt
@@ -1,3 +1,4 @@
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -49,4 +50,49 @@ internal class Day18ManyWorldsTest {
         val map = Files.lines(Paths.get("./src/test/resources/day18.txt")).toList()
         assertEquals(3646, Day18ManyWorlds(map).shortestPathToCollectAllKeys())
     }
+
+    @ParameterizedTest
+    @CsvSource(
+        "ö#######\\n" +
+                "#a.#Cd#\\n" +
+                "##@#@##\\n" +
+                "#######\\n" +
+                "##@#@##\\n" +
+                "#cB#Ab#\\n" +
+                "#######,8",
+        "ö###############\\n" +
+                "#d.ABC.#.....a#\\n" +
+                "######@#@######\\n" +
+                "###############\\n" +
+                "######@#@######\\n" +
+                "#b.....#.....c#\\n" +
+                "###############,24",
+        "ö#############\\n" +
+                "#DcBa.#.GhKl#\\n" +
+                "#.###@#@#I###\\n" +
+                "#e#d#####j#k#\\n" +
+                "###C#@#@###J#\\n" +
+                "#fEbA.#.FgHi#\\n" +
+                "#############,32",
+        "ö#############\\n" +
+                "#g#f.D#..h#l#\\n" +
+                "#F###e#E###.#\\n" +
+                "#dCba@#@BcIJ#\\n" +
+                "#############\\n" +
+                "#nK.L@#@G...#\\n" +
+                "#M###N#H###.#\\n" +
+                "#o#m..#i#jk.#\\n" +
+                "#############,72"
+    )
+    fun collectKeysInMultipleDungeonsExamples(rawMap: String, expected: Int) {
+        val map = rawMap.replace("ö", "").split("\\n")
+        assertEquals(expected, Day18ManyWorlds(map).shortestPathToCollectAllKeys())
+    }
+
+    @Test
+    fun collectKeysInMultipleDungeons() {
+        val map = Files.lines(Paths.get("./src/test/resources/day18_2.txt")).toList()
+        assertEquals(1730, Day18ManyWorlds(map).shortestPathToCollectAllKeys())
+    }
+
 }
